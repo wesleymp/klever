aeaa6615c32cd05f198a03dcd5406c27b659ec14
wesleymp <wesleyxd@hotmail.com>
2022-05-06 16:31:33 -0300

    refactor: add balance in data details

--- src/services/details.js
+++ src/services/details.js
@@ -1,16 +1,15 @@
 /* eslint-disable no-dupe-keys */
 const axios = require('../models/axios');
+require('dotenv').config();
 
 const details = async (address) => {
   const { data: dataDetails } = await axios.get(`/address/${address}`);
+  const { data: dataBalance } = await axios.get(`${process.env.BASE_URL}/balance/${address}`);
   const newDetails = {
     address: dataDetails.address,
     balance: dataDetails.balance,
     totalTx: dataDetails.txs,
-    balance_: {
-      confirmed: '',
-      unconfirmed: '',
-    },
+    balance_: dataBalance,
     total: {
       sent: dataDetails.totalSent,
       received: dataDetails.totalReceived,
