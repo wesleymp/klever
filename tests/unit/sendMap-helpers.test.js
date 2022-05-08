c725a19ae163f6ac60756bf3f422c9e49e190534
wesleymp <wesleyxd@hotmail.com>
2022-05-08 12:53:34 -0300

    test: created unit test to sendMap helper

---
+++ tests/unit/sendMap-helpers.test.js
@@ -0,0 +1,29 @@
+const sendMap = require('../../src/services/helpers/sendMap');
+const utxo = require('../memory-requests/utxo');
+
+describe('Helper sendMap', () => {
+  it('should return a json with keys correctly', () => {
+    const sendData = sendMap(utxo);
+    const expectSend = {
+      utxos: [
+        {
+          txid: 'd36d9bf2cd44dfb654cb9121add698334104feb9ee61c07e048369aa88b8462d',
+          amount: '1526092',
+        },
+        {
+          txid: 'e25fd45748afa9d19435b3716c37140b60be9a9075947e44c02a90214fb26ffd',
+          amount: '78297',
+        },
+        {
+          txid: '2199384a9ac83750ed2c5bf3bc3d22a64ca9fced53fafbabf73c0595ccab830c',
+          amount: '2715645',
+        },
+        {
+          txid: '5606c162193a8d1cb64f9e452e1ff09cc7a16240fb0357410e07488ce7c2ecf4',
+          amount: '2904394',
+        },
+      ],
+    };
+    expect(sendData).toMatchObject(expectSend);
+  });
+});
