095fa8508295dc8612e4517fe7171243f0271d59
wesleymp <wesleyxd@hotmail.com>
2022-05-19 16:09:59 -0300

    feat: add error connection aborted

--- src/middlewares/error.js
+++ src/middlewares/error.js
@@ -1,5 +1,8 @@
 const error = (err, _req, res, _next) => {
-  if (err.response.status < 500) {
+  if (err.code === 'ECONNABORTED') {
+    return res.status(500).json({ message: 'Server timeout' });
+  }
+  if (err.response.status !== 500) {
     return res.status(err.response.status).json({ message: err.response.data.error });
   }
   return res.status(500).json({ message: 'Internal error' });
